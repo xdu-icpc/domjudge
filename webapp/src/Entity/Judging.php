2a6ac355e6d75c5cfa03734717e1d56b4ca7c4a9
Xℹ Ruoyao <xry111@mengyan1223.wang>
2021-03-24 18:16:34 +0800

    ioi-mode: fix Judging::getScore(), again

--- webapp/src/Entity/Judging.php
+++ webapp/src/Entity/Judging.php
@@ -227,7 +227,7 @@ class Judging extends BaseApiEntity implements ExternalRelationshipEntityInterfa
      */
     private $runs;
 
-    private $score;
+    private $score = [0, 0];
 
     /**
      * Get the max runtime for this judging
@@ -789,7 +789,6 @@ class Judging extends BaseApiEntity implements ExternalRelationshipEntityInterfa
     public function __construct()
     {
         $this->runs = new \Doctrine\Common\Collections\ArrayCollection();
-        $this->score = [0, 0];
     }
 
     /**
